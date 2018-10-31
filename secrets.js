3a1afdbdb9d8f8313f413721b8dfe59ed9f5ef6a
Blessing Orazulume <orazulumeblessing@gmail.com>
2018-10-31 21:11:46 +0100

    updated git ignore

--- secrets.js
+++ secrets.js
@@ -1,6 +1,9 @@
 const SECRETS = {
     stripeSecret: 'sk_test_30bYqrngVY2c8XnaGtno71Z4',
-    hashingSecret: 'thisIsASecret'
+    hashingSecret: 'thisIsASecret',
+    mailGunSecret: 'api:093cb36660683bd99c4b21d2a35a4175-c9270c97-be9ceb14',
+    fromMail: 'postmaster@sandbox6c75f9a0312c4c4d995ec8bf8d80f734.mailgun.org',
+    mailGunPath: 'sandbox6c75f9a0312c4c4d995ec8bf8d80f734'
 }
 
 module.exports = SECRETS
